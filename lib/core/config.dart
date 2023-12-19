4675cdf25922941107033ca12980d8d363d4ae7e
ilhamelhamdi <ilhamabdillah123@gmail.com>
2023-12-19 13:55:10 +0700

    add default cover image & fix user initialization

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -3,3 +3,5 @@ const BASE_URL = 'https://bukoo.azurewebsites.net';
 
 const ProfilePictureDefault =
     'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg';
+
+const BookCoverDefault = "https://twinklelearning.in/uploads/noimage.jpg";
