f6b14a0505d767ca05847b6ad08db07897547028
Ilham Abdillah Alhamdi <63908394+ilhamelhamdi@users.noreply.github.com>
2023-12-20 15:06:32 +0700

    Revert "Feat/user profile"

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ import 'book_collection/screens/home_page.dart';
 import 'package:bukoo/admin_dashboard/admin_dash.dart';
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:bukoo/user_profile/screens/reader_profile.dart';
 
 void main() {
   runApp(const App());
@@ -61,12 +60,12 @@ class App extends StatelessWidget {
         ),
         initialRoute: HomePage.routeName,
         routes: {
-          HomePage.routeName: (context) => HomePage(),
-          '/admin_dashboard': (context) => AdminDash(),
-          LoginPage.routeName: (context) => LoginPage(),
-          RegisterPage.routeName: (context) => RegisterPage(),
-          /* ReaderProfile.routeName: (context) => ReaderProfilePage(), */
-
+          HomePage.routeName: (context) => const HomePage(),
+          '/admin_dashboard': (context) => const AdminDash(),
+          LoginPage.routeName: (context) => const LoginPage(),
+          RegisterPage.routeName: (context) => const RegisterPage(),
+          BookSubmissionPage.routeName: (context) => const BookSubmissionPage(),
+          SearchPage.routeName: (context) => const SearchPage(),
         },
       ),
     );
