aa2c3772586bb41a4ebe3b7ed63017edbeae8206
ilhamelhamdi <ilhamabdillah123@gmail.com>
2023-12-21 00:38:18 +0700

    Refactor book card layout and add scroll controller

--- lib/book_collection/widgets/scrollable_cards_wrapper.dart
+++ lib/book_collection/widgets/scrollable_cards_wrapper.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-class ScrollableCardsWrapper extends StatelessWidget {
+class ScrollableCardsWrapper extends StatefulWidget {
   final List<Widget> bookCards;
   final String title;
   const ScrollableCardsWrapper(
       {super.key, required this.bookCards, required this.title});
+
+  @override
+  State<ScrollableCardsWrapper> createState() => _ScrollableCardsWrapperState();
+}
+
+class _ScrollableCardsWrapperState extends State<ScrollableCardsWrapper> {
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -14,20 +21,22 @@ class ScrollableCardsWrapper extends StatelessWidget {
         Container(
           margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           child: Text(
-            title,
+            widget.title,
             style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
           ),
         ),
         Scrollbar(
+          controller: _scrollController,
           radius: const Radius.circular(4.0),
           child: Padding(
             padding: const EdgeInsets.only(bottom: 8.0),
             child: SingleChildScrollView(
               scrollDirection: Axis.horizontal,
+              controller: _scrollController,
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8),
                 child: Row(
-                  children: bookCards
+                  children: widget.bookCards
                       .map((card) => Padding(
                             padding: const EdgeInsets.symmetric(horizontal: 8),
                             child: card,
